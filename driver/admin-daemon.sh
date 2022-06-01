0b8eb85b53828bff26de17d71cd4c1f832055583
shinsaku.yamamura <shinsaku.yamamura>
2022-06-01 18:57:50 +0900

    Sync by Darwin in gretel@gn-cl-06.local

--- driver/admin-daemon.sh
+++ driver/admin-daemon.sh
@@ -7,13 +7,11 @@ PYTHON_PATH="/home/gretel/.pyenv/versions/TempKeeper/bin/python"
 # migration
 docker-compose exec driver alembic upgrade head
 
-while true
+while true; do
     # sensor function
     sudo ${PYTHON_PATH} ./temp_collector.py
 
     # plug function
     ${PYTHON_PATH} ./plug_controller.py
-
-do
 
 done
