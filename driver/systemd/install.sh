cdbdd76911ba3af40b40e283fc50bca10efb2022
shinsaku.yamamura <shinsaku.yamamura>
2022-06-01 19:15:39 +0900

    Sync by Darwin in gretel@gn-cl-06.local

--- driver/systemd/install.sh
+++ driver/systemd/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SERVICE_NAME="TempKeeperAdminService"
-ln -s "/home/gretel/${SERVICE_NAME}/driver/systemd/${SERVICE_NAME}.service" "/usr/lib/systemd/system/${SERVICE_NAME}.service"
+ln -s "/home/gretel/TempKeeper/driver/systemd/${SERVICE_NAME}.service" "/usr/lib/systemd/system/${SERVICE_NAME}.service"
 systemctl daemon-reload
 systemctl enable "${SERVICE_NAME}.service"
