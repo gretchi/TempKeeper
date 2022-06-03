1cef388db1f780e0d52fc6f28017bb3fc5ea1d43
shinsaku.yamamura <shinsaku.yamamura>
2022-06-04 05:05:04 +0900

    Sync by Darwin in gretel@gn-cl-06.local

---
+++ driver/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from posixpath import split
+from flask import Flask, render_template, request, redirect
+from pprint import pprint
+
+from model import Model
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    model = Model()
+    nodes = model.get_nodes()
+    model.close()
+    return render_template('index.html', nodes=nodes)
+
+
+@app.route('/set-preset-temp', methods=['POST'])
+def set_preset_temp():
+    model = Model()
+    for k, v in request.form.items():
+        prefix, idx = k.split("-")
+        if prefix != "preset_temp":
+            continue
+
+        model.set_preset_temp(int(idx), float(v))
+
+    model.commit()
+    model.close()
+
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8888)
