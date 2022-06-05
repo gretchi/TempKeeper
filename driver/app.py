7c7e7d0d34f1a286dfa1a6c94d3320f60d23c455
shinsaku.yamamura <shinsaku.yamamura>
2022-06-05 16:19:11 +0900

    Sync by Darwin in gretel@gn-cl-06.local

--- driver/app.py
+++ driver/app.py
@@ -17,12 +17,39 @@ def index():
     return render_template('index.html', nodes=nodes)
 
 
+@app.route('/set-node', methods=['POST'])
+def set_node():
+    model = Model()
+    form_dict = {}
+
+    for k, v in request.form.items():
+        prop_name, idx = k.split("-")
+
+        if idx not in form_dict:
+            form_dict[idx] = {}
+
+        form_dict[idx][prop_name] = v
+
+    for idx, row in form_dict.items():
+        sensor_mac = row["sensor_mac"]
+        plug_mac = row["plug_mac"]
+        preset_temp = row["preset_temp"]
+        location_name = row["location_name"]
+
+        model.set_node(idx, sensor_mac, plug_mac, preset_temp, location_name)
+
+    model.commit()
+    model.close()
+
+    return redirect("/")
+
+
 @app.route('/set-preset-temp', methods=['POST'])
 def set_preset_temp():
     model = Model()
     for k, v in request.form.items():
-        prefix, idx = k.split("-")
-        if prefix != "preset_temp":
+        prop_name, idx = k.split("-")
+        if prop_name != "preset_temp":
             continue
 
         model.set_preset_temp(idx, v)
